c5f6ca6baf08b1a52e7355231d8bf9e6a5293e08
sanjida-06 <sanjidaaktersumaiya33@gmail.com>
2026-09-20 07:23:40 +0600

    main section setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import Navbar from "./components/shared/Navbar";
 import Hero from "./components/shared/Hero";
 import Footer from "./components/shared/Footer";
+import Cards from "./components/CardsSection/Cards";
+
+const fetchCards = async () => {
+  const res = await fetch("/cards.json");
+  const data = await res.json();
+  return data; 
+}
+const cardsPromise = fetchCards();
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <main>
+        <section className='container mx-auto my-10'>
+          <div>
+            {/*cards component*/}
+            <Cards />
+            {/*list component*/}
+          </div>
+          </section>
+      </main>
       <Footer />
     </div>
   );
